d867f3bcdb850a1f2e799bcf727195fd867ca8f8
Martijn van der Kleijn <martijn.niji@gmail.com>
2023-06-06 23:42:40 +0200

    fix some spacing issues

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 /*
 	HomeDash - A simple, automated dashboard for home labs.
-    Copyright (C) 2023  Martijn van der Kleijn
+	Copyright (C) 2023  Martijn van der Kleijn
 
 	This file is part of HomeDash.
 
-    This Source Code Form is subject to the terms of the Mozilla Public
-  	License, v. 2.0. If a copy of the MPL was not distributed with this
-  	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 package main
