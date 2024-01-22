f30fbef469233c18e4bb9ce2dce6150925379fab
Martijn van der Kleijn <martijn.niji@gmail.com>
2024-01-22 16:24:18 +0100

    Add missing license headers

--- internal/repositories/app.go
+++ internal/repositories/app.go
@@ -1,3 +1,14 @@
+/*
+	HomeDash - A simple, automated dashboard for home labs.
+	Copyright (C) 2023  Martijn van der Kleijn
+
+	This file is part of HomeDash.
+
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 package repositories
 
 import (
